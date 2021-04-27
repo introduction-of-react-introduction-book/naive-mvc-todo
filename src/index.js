154337bb79a41c068fe92292fcb034ce7c4d3232
sadness_ojisan <sadness.ojisan@gmail.com>
2021-04-27 13:36:03 +0900

    add todo

--- src/index.js
+++ src/index.js
@@ -1,50 +1,122 @@
-console.log("hello world!!");
+const todos = {};
 
-/**
- * formの送信時に実行する関数。
- * input form の入力内容を取得してそれをTODOリストに加える
- */
-function handleFormSubmit() {
-  const input = document.getElementById("task-input");
-  const inputValue = input.value; // 厳密には instanceof で input要素であるかのチェックを入れるべき
-  if (!inputValue.length > 0) {
-    alert("テキストを入力してください。");
-    return;
+class TodoListModel {
+  constructor() {
+    this.idCounter = 0;
+    this.todos = new Map();
   }
-  const todosEl = document.getElementById("todos");
-  const todoEl = createTodoElement(inputValue);
-  todosEl.appendChild(todoEl);
-  input.value = ""; // input のリセット
+
+  /**
+   * task を todo として todoList に追加する
+   * @param {string} task
+   * @returns 追加された todo の id
+   */
+  addTodo(task) {
+    this.idCounter += 1;
+    this.todos.set(this.idCounter, { id: this.idCounter, task });
+    return this.idCounter;
+  }
+
+  removeTodo() {}
+
+  checkTodo() {}
+
+  getTodos() {
+    return Array.from(this.todos.values());
+  }
+
+  getTodo(id) {
+    return this.todos.get(id);
+  }
 }
 
-/**
- * TODO要素を作る関数
- * @param {*} inputValue TODO文字列
- * @returns TODO要素
- */
-function createTodoElement(inputValue) {
-  const todoEl = document.createElement("li");
-  const checkBoxEl = document.createElement("input");
-  todoEl.appendChild(checkBoxEl);
-  const labelEl = document.createElement("label");
-  labelEl.innerText = inputValue;
-  checkBoxEl.type = "checkbox";
-  checkBoxEl.onchange = function (e) {
-    const checked = e.target.checked;
-    if (checked) {
-      todoEl.className = `checked`;
-    } else {
-      todoEl.className = "";
-    }
-  };
-  todoEl.appendChild(labelEl);
+const todoList = new TodoListModel();
 
-  const buttonEl = document.createElement("button");
-  buttonEl.innerText = "削除";
-  buttonEl.onclick = function () {
-    todoEl.remove();
-  };
-  todoEl.appendChild(buttonEl);
+class View {
+  render(todos) {}
 
-  return todoEl;
+  /**
+   *
+   * @param {id: number, task: string} todo
+   */
+  addTodo(todo) {
+    const todosEl = document.getElementById("todos");
+    const todoEl = this._createTodoElement(todo.task);
+    todosEl.appendChild(todoEl);
+  }
+
+  // checkbox をいじる
+  // react化すると不要
+  check() {}
+
+  resetTodo() {
+    const input = document.getElementById("task-input");
+    input.value = ""; // input のリセット
+  }
+
+  /**
+   *
+   * @param {string} task
+   * @returns todoのHTML要素
+   */
+  _createTodoElement(task) {
+    const todoEl = document.createElement("li");
+    const checkBoxEl = document.createElement("input");
+    todoEl.appendChild(checkBoxEl);
+    const labelEl = document.createElement("label");
+    labelEl.innerText = task;
+    checkBoxEl.type = "checkbox";
+    checkBoxEl.onchange = function (e) {
+      const checked = e.target.checked;
+      if (checked) {
+        todoEl.className = `checked`;
+      } else {
+        todoEl.className = "";
+      }
+    };
+    todoEl.appendChild(labelEl);
+
+    const buttonEl = document.createElement("button");
+    buttonEl.innerText = "削除";
+    buttonEl.onclick = function () {
+      todoEl.remove();
+    };
+    todoEl.appendChild(buttonEl);
+
+    return todoEl;
+  }
 }
+
+const view = new View();
+
+class Controller {
+  setup() {
+    this.handleSubmitForm();
+    this.handleCheckTask();
+    this.handleClickDeleteTask();
+  }
+
+  handleSubmitForm() {
+    const formEl = document.getElementById("task-send-form");
+    formEl.addEventListener("submit", function (ev) {
+      ev.preventDefault();
+
+      const input = document.getElementById("task-input");
+      const task = input.value;
+      if (!task.length > 0) {
+        alert("テキストを入力してください。");
+        return;
+      }
+      const addedTodoId = todoList.addTodo(task);
+      const todo = todoList.getTodo(addedTodoId);
+      view.addTodo(todo);
+    });
+  }
+
+  handleCheckTask(id) {}
+
+  handleClickDeleteTask() {}
+}
+
+const formController = new Controller();
+formController.setup();
