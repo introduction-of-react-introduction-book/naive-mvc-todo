41f1f367bfad1708c7fefbd92d08cf592fcca38b
sadness_ojisan <sadness.ojisan@gmail.com>
2021-04-27 14:43:42 +0900

    update doc

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ class TodoListModel {
   }
 
   /**
-   * task を todo として todoList に追加する
+   * task を todo として todoList に追加する.
    * @param {string} task
    * @returns 追加された todo の id
    */
@@ -19,16 +19,31 @@ class TodoListModel {
     return this.idCounter;
   }
 
+  /**
+   * 指定idのTODOを削除
+   * @param {number} id TODOのid
+   */
   removeTodo(id) {
     this.todos.delete(id);
   }
 
+  /**
+   * TODOの完了状態を変更する
+   * @param {number} id TODOのid
+   * @param {boolean} isCheck 次のcheck状態
+   * @returns 更新済みTODO
+   */
   checkTodo(id, isCheck) {
     const todo = this.todos.get(id);
     todo.checked = isCheck;
     return todo;
   }
 
+  /**
+   * 指定したidのTODOを取得
+   * @param {number} id TODOのid
+   * @returns TODO
+   */
   getTodo(id) {
     return this.todos.get(id);
   }
@@ -38,8 +53,8 @@ const todoList = new TodoListModel();
 
 class View {
   /**
-   *
-   * @param {id: number, task: string} todo
+   * TODOをUIに追加する
+   * @param {id: number, task: string, isCheck: boolean} todo
    */
   addTodo(todo) {
     const todosEl = document.getElementById("todos");
@@ -47,30 +62,43 @@ class View {
     todosEl.appendChild(todoEl);
   }
 
-  // checkbox をいじる
-  // react化すると不要
+  /**
+   * todoをチェックに応じてスタイルを変える
+   * @param {*} id TODOのid
+   */
   check(id) {
     const todoEl = document.getElementById(`todo-${id}`);
     todoEl.className = `checked`;
   }
 
+  /**
+   * todoをチェックに応じてスタイルを変える
+   * @param {*} id TODOのid
+   */
   unCheck(id) {
     const todoEl = document.getElementById(`todo-${id}`);
     todoEl.className = "";
   }
 
+  /**
+   * input form をリセットする
+   */
   resetTodo() {
     const input = document.getElementById("task-input");
     input.value = ""; // input のリセット
   }
 
+  /**
+   * 指定したTODOをDOMから削除する
+   * @param {*} id TODOのid
+   */
   removeTodo(id) {
     const todoEl = document.getElementById(`todo-${id}`);
     todoEl.remove();
   }
 
   /**
-   *
+   * TODO要素を作る
    * @param {id: number, task: string} todo
    * @returns todoのHTML要素
    */
@@ -105,6 +133,9 @@ class Controller {
     this.handleClickDeleteTask();
   }
 
+  /**
+   * タスク送信時にTODO追加とUI反映をする
+   */
   handleSubmitForm() {
     const formEl = document.getElementById("task-send-form");
     formEl.addEventListener("submit", (ev) => {
@@ -123,6 +154,10 @@ class Controller {
     });
   }
 
+  /**
+   * 指定したcheckboxの状態変化に応じてTODO更新とUI反映をする
+   * @param {number} id TODOのid
+   */
   handleCheckTask(id) {
     const checkBoxEl = document.getElementById(`checkbox-${id}`);
     checkBoxEl.onchange = function (e) {
@@ -136,6 +171,10 @@ class Controller {
     };
   }
 
+  /**
+   * 指定したTODO削除とUI反映をする
+   * @param {*} id TODOのid
+   */
   handleClickDeleteTask(id) {
     todoList.removeTodo(id);
     const buttonEl = document.getElementById(`button-${id}`);
